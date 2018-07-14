3748f187c6b9ccf77d1d8ee260292a28ecf7ad62
Ego Dafma Dasa <egodasa@gmail.com>
2018-07-14 23:46:37 +0700

    Memindahkan index.html di root

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
   },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
-  },
   optimization: {
     minimize: true,
   },
